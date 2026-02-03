98e14287743b661332f66ec76f14f8a38291379d
Fidel Yiu <fidel-yiu@foxmail.com>
2026-02-03 23:06:55 +0800

    docs: 修改文档

--- packages/docs/config/i18n.ts
+++ packages/docs/config/i18n.ts
@@ -2,5 +2,5 @@ import type { Config } from "@docusaurus/types";
 
 export const i18nConfig: Config["i18n"] = {
   defaultLocale: "zh-Hans",
-  locales: ["zh-Hans"],
+  locales: ["zh-Hans", "en"],
 };
