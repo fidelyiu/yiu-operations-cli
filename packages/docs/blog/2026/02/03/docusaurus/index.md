27de60d18227acb9da211f6c664964b7f7407239
Fidel Yiu <fidel-yiu@foxmail.com>
2026-02-03 22:01:54 +0800

    docs: 修改文档

---
+++ packages/docs/blog/2026/02/03/docusaurus/index.md
@@ -0,0 +1,14 @@
+---
+slug: docusaurus
+title: Docusaurus 使用
+authors: [FidelYiu]
+tag: [docusaurus]
+---
+
+# Docusaurus 使用
+
+本 web 站点使用 [docusaurus](https://docusaurus.io/) 搭建
+
+<!-- truncate -->
+
+基本操作请参考 [案例-docusaurus](/examples/docusaurus/intro)，更多信息参考 [docusaurus 官网](https://docusaurus.io/)
--- packages/docs/config/presets.ts
+++ packages/docs/config/presets.ts
@@ -20,8 +20,8 @@ export const presetsConfig: Config["presets"] = [
         },
         // Please change this to your repo.
         // Remove this to remove the "edit this page" links.
-        editUrl:
-          "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // editUrl:
+        //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         // Useful options to enforce blogging best practices
         onInlineTags: "warn",
         onInlineAuthors: "warn",
